f17cb978efbff26a70971e38fe157a00b6261e0c
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 16:55:05 -0400

    Add files via upload

---
+++ weight_insultation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int weight;
+    weight = 138;
+    
+    printf("Today your weight amounts to %d. \n", weight);
+    printf("If you had enough motivation to excercise, you'd gain 30 pounds of muscle. \n");
+    printf("Then your weight would be %d, stick-bug. \n", weight + 30);
+    return(0);
+}
