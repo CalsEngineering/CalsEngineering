29c6a9780ef7f9db5ee715ac532843a4be49bcd4
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 16:49:18 -0400

    Add files via upload

---
+++ age.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    age = 43;   /* put your age here. */
+    printf("You are...%d years old. \n", age);
+    printf("In five years you will be...%d years old. \n", age+5);
+    printf("In 20 years you will be...%d years old. \n", age+20);
+    printf("But for now you are just %d. \n", age);
+    return(0);
+}
