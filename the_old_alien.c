8b8e9bad09776e8703e467532aeb9b74a747c1de
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 16:43:08 -0400

    Add files via upload

---
+++ the_old_alien.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float distance;
+    
+    age = 2123;
+    distance = 319.215;
+    
+    printf("The alien is %d years old and ", age);
+    printf("comes from a planet that is %f microns away.\n", distance);
+    return(0);
+}
