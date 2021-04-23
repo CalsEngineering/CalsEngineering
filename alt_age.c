8eaf166f3c7c875237cbb447f8b503f7a5e66589
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 16:51:36 -0400

    Add files via upload

---
+++ alt_age.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    age = 23; /* put your age here. */
+    printf("You are %d years old. \n", age);
+    age = age + 5;
+    printf("Five years from now, you will be %d years old. \n", age);
+    return(0);
+}
