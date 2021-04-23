9d70fed613942ba190b19eaa3ad4f35422501a70
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 16:06:02 -0400

    Add files via upload

---
+++ soloist.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The First Soloist\n\n");  /*extra blank line*/
+    printf("Vocalist Mary McDiva\n");
+ /* printf("Song, 'Under The Sea.'\n"); */
+    return(0);
+    
+}
+
+
