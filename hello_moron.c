4bc183266d93f4e868a18e842cfcc96a84e163cb
CalsEngineering <calsengineering1@gmail.com>
2021-04-23 18:05:42 -0400

    Add files via upload

---
+++ hello_moron.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    putchar('H');
+    putchar('E');
+    putchar('L');
+    putchar('L');
+    putchar('O');
+    putchar(' ');
+    putchar('M');
+    putchar('O');
+    putchar('R');
+    putchar('O');
+    putchar('N');
+    return(0);
+}
