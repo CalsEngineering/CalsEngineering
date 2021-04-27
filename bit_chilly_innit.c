b0b78b653f975de2a39e4cc9d923df3cf1f3d01e
CalsEngineering <calsengineering1@gmail.com>
2021-04-27 13:14:47 -0400

    Add files via upload

---
+++ bit_chilly_innit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float temp;
+    
+    printf("What is the temperature outside?");
+    scanf("%f", &temp);
+    if(temp < 65);
+    {
+        printf("It's a bit chilly out, innit? \n");
+    }
+    return(0);
+}
